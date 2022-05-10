c99ece6543e6664f7657039efca3281b12ea0327
a-guinea-pig <105298312+a-guinea-pig@users.noreply.github.com>
2022-05-10 23:29:03 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -25,8 +25,12 @@ with st.echo(code_location='below'):
 
     dataset
     
-    st.image(dataset.groupby(['country']).sum().plot(
-    kind='pie', y='lines', autopct='%1.0f%%'))
+    colorss = ['#E08DAC', '#6A7FDB', '#57E2E5', '#45CB85', '#F9C846', '#F26419']
+    df = dataset[["systems", "region"]]
+    df1 = df.groupby(['region']).sum()
+    df2 = df1.sort_values(by=["systems"], ascending=True)
+    plot = df2.plot.pie(y='systems', figsize=(10, 10), wedgeprops = { 'linewidth' : 3, 'edgecolor' : 'white' }, colors=colorss)
+
     
     
 
