6dc525b1d9dcfec7d15ab32c60c12ad13c0c00c3
a-guinea-pig <105298312+a-guinea-pig@users.noreply.github.com>
2022-05-10 19:36:58 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,76 +6,29 @@ import matplotlib.pyplot as plt
 import altair as alt
 
 with st.echo(code_location='below'):
-    """
-    ## Hello, World!
-    """
+
+    import streamlit as st
+    import folium
+    import pandas as pd
 
 
-    def print_hello(name="World"):
-        st.write(f"### Hello, {name}!")
+    def print_hello(name = "World"):
+        st.write(f"## Hello, {name}!")
 
-
-    name = st.text_input("Your name", key="name", value="Anonymous")
+    name = st.text_area("enter your name")
     print_hello(name)
 
-    """
-    ## Добавим графики
-    Чтобы заработали библиотеки seaborn и altair, нужно добавить в проект файл 
-    `requirements.txt` с такими строчками:
-    
-        seaborn
-        altair
-    """
-
-
-    a = st.slider("a")
-    x = np.linspace(-6, 6, 500)
-    df = pd.DataFrame(dict(x=x, y=np.sin(a * x)))
-    fig, ax = plt.subplots()
-    sns.lineplot(data=df, x="x", y="y", ax=ax)
-    st.pyplot(fig)
-
-    """
-    ## Немного анализа данных
-    """
-
-
     @st.cache
-    def get_data():
-        data_url = (
-            "https://github.com/Godoy/imdb-5000-movie-dataset/raw/"
-            "master/data/movie_metadata.csv"
-        )
-        return (
-            pd.read_csv(data_url)
-            .dropna(subset=["title_year"])
-            .assign(
-                title_year=lambda x: pd.to_datetime(
-                    x["title_year"], format="%Y"
-                )
-            )
-        )
+    def get_dataset():
+        dataset_link = "metro_countries_total.csv"
+        return pd.read_csv(dataset_link)
 
+    dataset = get_dataset()
 
-    df = get_data()
+    dataset
 
-    director = st.selectbox(
-        "Director", df["director_name"].value_counts().iloc[:10].index
-    )
+    #from https://python-graph-gallery.com/313-bubble-map-with-folium
 
-    df_selection = df[lambda x: x["director_name"] == director]
-    df_selection
+    m = folium.Map(location=[0,0])
 
-    chart = (
-        alt.Chart(df_selection)
-        .mark_circle()
-        .encode(x=alt.X("title_year:T"), y="imdb_score", tooltip="movie_title")
-    )
-
-    st.altair_chart(
-        (
-            chart
-            + chart.transform_loess("title_year", "imdb_score").mark_line()
-        ).interactive()
-        # .transform_loess добавляет сглаживающую кривую
-    )
+    m
