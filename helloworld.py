5d41392deddaa35c9ac714d23a291cc0c04dbf62
a-guinea-pig <105298312+a-guinea-pig@users.noreply.github.com>
2022-05-10 22:10:52 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -21,29 +21,3 @@ with st.echo(code_location='below'):
 
     dataset
 
-# Library
-library(leaflet)
- 
-# load example data (Fiji Earthquakes) + keep only 100 first lines
-data(quakes)
-quakes =  head(quakes, 100)
- 
-# Create a color palette with handmade bins.
-mybins=seq(4, 6.5, by=0.5)
-mypalette = colorBin( palette="YlOrBr", domain=quakes$mag, na.color="transparent", bins=mybins)
- 
-# Prepare the text for the tooltip:
-mytext=paste("Depth: ", quakes$depth, "<br/>", "Stations: ", quakes$stations, "<br/>", "Magnitude: ", quakes$mag, sep="") %>%
-  lapply(htmltools::HTML)
- 
-# Final Map
-leaflet(quakes) %>% 
-  addTiles()  %>% 
-  setView( lat=-27, lng=170 , zoom=4) %>%
-  addProviderTiles("Esri.WorldImagery") %>%
-  addCircles(~long, ~lat, 
-    fillColor = ~mypalette(mag), fillOpacity = 0.7, color="white", radius=~sqrt(depth)*3000, stroke=FALSE, weight = 1,
-    label = mytext,
-    labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
-  ) %>%
-  addLegend( pal=mypalette, values=~mag, opacity=0.9, title = "Magnitude", position = "bottomright" )
